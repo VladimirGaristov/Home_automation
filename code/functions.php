99d971ea83fe838def2d15b0b410b0cf66e4c75d
Vlado <vladimir.garistov.42@gmail.com>
2017-04-17 04:39:54 +0300

    Сериозен напредък по back-end–а на сървъра. Работи свързването на нов модул.

--- code/functions.php
+++ code/functions.php
@@ -1,61 +1,210 @@
 <?php
 
 const S_PORT_NAME = '/dev/ttyACM0';
+const BAUD_RATE = 9600;
+const READ_REQUEST = '?';
+const DELAY = 50000;
+const DB_USERNAME = 'kompir';
+const DB_PASS = 'chumbedrum420';
+const DB_NAME = 'home_automation';
 
-function serial_init($port_name)
+class Comm_protocol_action
 {
-	$ser_port_opt=array('baud'=>9600, 'bits'=>8, 'stop'=>1, 'parity'=>0);
-	$arduino=dio_open(S_PORT_NAME, O_RDWR);
-	dio_tcsetattr($arduino, $ser_port_opt);
-	//usleep(2000);
-	return $arduino;
+	public $command;
+	public $IP;
+	public $type;
+	public $reply;
+
+	function command_type()
+	{
+		switch($this->get_param())
+		{
+			case 'H':
+				return 'send_IP';
+				break;
+			case 'I':
+				return 'new_module';
+				break;
+			case 'D':
+				return 'var_define';
+				break;
+			case 'W':
+				return 'db_write';
+				break;
+			case 'R':
+				return 'db_read';
+				break;
+			default:
+				return 1;
+		}
+	}
+
+	function new_module()
+	{
+		global $db;
+		$new_module_name=$this->get_param();
+		if($new_module_name===-1)
+			$this->error(10);
+		else
+		{
+			//проверка
+			$modules_with_this_name=$db->query("SELECT * FROM modules WHERE name='".$new_module_name."'");
+			if($modules_with_this_name->num_rows!=0)
+				$this->error(7);
+			else
+			{
+				$db->query("CREATE TABLE `home_automation`.`".$new_module_name."` ( `id` INT NOT NULL AUTO_INCREMENT , `variable_name` VARCHAR(256) NOT NULL , `permissions` TINYINT NOT NULL , `value` VARCHAR(32767) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+				$db->query("INSERT INTO `modules` (`id`, `name`, `IP`) VALUES (NULL, '".$new_module_name."', '".$this->IP."')");
+				$this->reply='###'.$this->IP.';A;Valid;###';
+			}
+		}
+	}
+
+	function db_write()
+	{
+		global $db;
+		/*
+		1 parse
+		2 check if exists
+		3 check permissions
+		4 write
+		*/
+	}
+
+	function db_read()
+	{
+		global $db;
+	}
+
+	function var_define()
+	{
+		global $db;
+	}
+
+	function error($code)
+	{
+		switch($code)
+		{
+			case 1:
+				$error_msg='Message corrupted or invalid';
+				break;
+			case 2:
+				$error_msg='Invalid syntax';
+				break;
+			case 3:
+				$error_msg='Permission denied';
+				break;
+			case 4:
+				$error_msg='Unknown command';
+				break;
+			case 5:
+				$error_msg='Nonexistent module';
+				break;
+			case 6:
+				$error_msg='Nonexistent varriable';
+				break;
+			case 7:
+				$error_msg='Module already exists';
+				break;
+			case 8:
+				$error_msg='Varriable already exists';
+				break;
+			case 9:
+				$error_msg='Invalid permission declaration';
+				break;
+			case 10:
+				$error_msg='Missing argument';
+				break;
+			case 11:
+				$error_msg='Invalid variable name';
+				break;
+			case 12:
+				$error_msg='Invalid module name';
+				break;
+			default:
+				$error_msg='Unknown error';
+				$code=13;
+		}
+		$this->reply='###'.$this->IP.';E;ERROR '.$code.': '.$error_msg.';###';	//$arduino->write
+	}
+
+	function send_IP()
+	{
+		$this->reply='###'.$this->IP.';H;'.$_SERVER['SERVER_ADDR'].';###';
+	}
+
+	function get_param()
+	{
+		$p=strpos($this->command, ';');
+		if($p===FALSE)
+			return -1;
+		else
+		{
+			$s=substr($this->command, 0, $p);
+			$this->command=substr($this->command, $p+1);
+			return $s;
+		}
+	}
+
+	function verify()
+	{
+		global $db;
+		$this->command=$db->real_escape_string($this->command);
+		if(strcmp(substr($this->command, 0, 3), substr($this->command, -3))==0)
+			return 0;
+		else
+			return 1;
+	}
+
+	function get_IP()
+	{
+		$this->command=substr($this->command, 3, -3);
+		$this->IP=$this->get_param();
+	}
+
+	//Добави проверки за имена и стойности!
 }
 
-function cancur(&$arduino)
+class Serial
 {
-	dio_write($arduino, '0');
-	usleep(50000);
-	echo dio_read($arduino, 100);
-}
+	public $conn;
 
-function s_read()
-{
+	function init($port_name)
+	{
+		$ser_port_opt=array('baud'=>BAUD_RATE, 'bits'=>8, 'stop'=>1, 'parity'=>0);
+		$this->conn=dio_open(S_PORT_NAME, O_RDWR);
+		dio_tcsetattr($this->conn, $ser_port_opt);
+		//usleep(2000);
+	}
 
-}
+	function end()
+	{
+		dio_close($this->conn);
+	}
 
-function s_write()
-{
+	function read(&$output)
+	{
+		dio_write($this->conn, READ_REQUEST);
+		usleep(DELAY);
+		$l=dio_read($this->conn,4);
+		if($l==0)
+			return -1;
+		$output=dio_read($this->conn, $l);
+		return $l;
+	}
 
-}
+	function write(&$input)
+	{
+		usleep(DELAY);
+		dio_write($this->conn, $input, strlen($input));
+	}
 
-function verify()
-{
-
-}
-
-function decode()
-{
-
-}
-
-function new_module()
-{
-
-}
-
-function db_write()
-{
-
-}
-
-function db_read()
-{
-
-}
-
-function respond()
-{
-
+	function cancur()
+	{
+		dio_write($this->conn, '0');
+		usleep(DELAY);
+		echo dio_read($this->conn, 100);
+	}
 }
 
 ?>
--- code/serial.php
+++ code/serial.php
@@ -3,10 +3,34 @@
 require_once('functions.php');
 set_time_limit(0);
 
-$arduino=serial_init(S_PORT_NAME);
+/*
+$arduino=new Serial();
+$arduino->init(PORT_NAME);
+$arduino->cancur();
+$arduino->end();
+*/
 
-cancur($arduino);
+$db=new mysqli("localhost", DB_USERNAME, DB_PASS, DB_NAME);
+if($db->connect_errno)
+	exit('Failed to connect to MySQL: ('.$db->connect_errno.') '.$db->connect_error);
 
-dio_close($arduino);
+$a=new Comm_protocol_action();
+$a->command='###192.168.1.4;I;karpuz;###';
+
+if($a->verify()===0)	//Това ще е в цикъл.
+{
+	$a->get_IP();
+	$a->type=$a->command_type();
+	if($a->type===1)
+		$a->error(4);
+	else
+	{
+		call_user_func(array($a, $a->type));
+		if(isset($a->reply))
+			echo $a->reply;	//$arduino->write
+	}
+}
+else
+	$a->error(1);
 
 ?>
