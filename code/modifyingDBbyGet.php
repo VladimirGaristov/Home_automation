77a26fb39609c06b397dcff37211527fbb55db00
Rangel Ivanov <radito3@mail.bg>
2017-04-20 10:42:00 +0300

    Update modifyingDBbyGet.php

--- code/modifyingDBbyGet.php
+++ code/modifyingDBbyGet.php
@@ -8,25 +8,29 @@
    define('DB_PASSWORD', 'chumbedrum420');
    define('DB_NAME', 'home_automation');
    $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+  
+   if (mysqli_connect_errno()) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   }
 
    class ModifyDB {
-       /*private $small_lamp;
-       private $pechka;
-       private $big_lamp;
-       private $_password;*/
+       private $small_lamp = mysqli_real_escape_string($db, $_GET['small_lamp_p']);
+       private $pechka = mysqli_real_escape_string($db, $_GET['pechka']);
+       private $big_lamp = mysqli_real_escape_string($db, $_GET['big_lamp']);
+       private $_password = mysqli_real_escape_string($db, $_GET['password']);
 
        public function writing_to_DB() {
-          if ($_GET['small_lamp_p']) {
-              $db->query("INSERT INTO `small_lamp_p` VALUES (".$_GET['small_lamp_p'].");");
+          if ($small_lamp) {
+              $db->query("INSERT INTO `small_lamp_p` VALUES (".$small_lamp.");");
           }
-          if ($_GET['pechka']) {
-              $db->query("INSERT INTO `pechka` VALUES (".$_GET['pechka'].");");
+          if ($pechka) {
+              $db->query("INSERT INTO `pechka` VALUES (".$pechka.");");
           }
-          if ($_GET['big_lamp']) {
-              $db->query("INSERT INTO `big_lamp` VALUES (".$_GET['big_lamp'].");");
+          if ($big_lamp) {
+              $db->query("INSERT INTO `big_lamp` VALUES (".$big_lamp.");");
           }
-          if ($_GET['password']) {
-              $db->query("INSERT INTO `password` VALUES (".$_GET['password'].");");
+          if ($_password) {
+              $db->query("INSERT INTO `password` VALUES (".$_password.");");
           }
        }
 
