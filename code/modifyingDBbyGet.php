39bd45806fd231c113fb755af48db8eed2d71abd
Rangel Ivanov <radito3@mail.bg>
2017-04-21 15:40:49 +0300

    Update modifyingDBbyGet.php

--- code/modifyingDBbyGet.php
+++ code/modifyingDBbyGet.php
@@ -8,8 +8,9 @@
    define('DB_USERNAME', 'kompir');
    define('DB_PASSWORD', 'chumbedrum420');
    define('DB_NAME', 'home_automation');
+   define('PASSWORD', 'Apple');
    $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-  
+
    if (mysqli_connect_errno()) {
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
    }
@@ -19,17 +20,16 @@
    $big_lamp = mysqli_real_escape_string($db, $_GET['big_lamp']);
    $_password = mysqli_real_escape_string($db, $_GET['password']);
 
-   if ($small_lamp) {
-       $db->query("UPDATE `".DB_NAME."` SET `small_lamp` = ".$small_lamp.";");
-   }
-   if ($pechka) {
-       $db->query("UPDATE `".DB_NAME."` SET `pechka` = ".$pechka.";");
-   }
-   if ($big_lamp) {
-       $db->query("UPDATE `".DB_NAME."` SET `big_lamp` = ".$big_lamp.";");
-   }
-   if ($_password) {
-       $db->query("UPDATE `".DB_NAME."` SET `password` = ".$_password.";");
+   if ($_password == PASSWORD) {
+       if ($small_lamp) {
+           $db->query("UPDATE `small_lamp` SET `on` = ".$small_lamp.";");
+       }
+       if ($pechka) {
+           $db->query("UPDATE `pechka` SET `power` = ".$pechka.";");
+       }
+       if ($big_lamp) {
+           $db->query("UPDATE `big_lamp` SET `big_lamp` = ".$big_lamp.";");
+       }
    }
 
    mysql_close($db);
