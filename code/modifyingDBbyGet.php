5e20db4c099854c74b0d0f67c35932178bece707
Rangel Ivanov <radito3@mail.bg>
2017-04-21 09:14:07 +0300

    Update modifyingDBbyGet.php
    Обновяване на обработването на GET заявки

--- code/modifyingDBbyGet.php
+++ code/modifyingDBbyGet.php
@@ -1,9 +1,10 @@
 //prototype code
 //Added security check for sql injection
+//Updated with proper sql commands
 //needs to be tested
 
 <?php
-   define('DB_SERVER', 'localhost:3036');
+   define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'kompir');
    define('DB_PASSWORD', 'chumbedrum420');
    define('DB_NAME', 'home_automation');
@@ -13,27 +14,22 @@
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
    }
 
-   class ModifyDB {
-       private $small_lamp = mysqli_real_escape_string($db, $_GET['small_lamp_p']);
-       private $pechka = mysqli_real_escape_string($db, $_GET['pechka']);
-       private $big_lamp = mysqli_real_escape_string($db, $_GET['big_lamp']);
-       private $_password = mysqli_real_escape_string($db, $_GET['password']);
+   $small_lamp = mysqli_real_escape_string($db, $_GET['small_lamp_p']);
+   $pechka = mysqli_real_escape_string($db, $_GET['pechka']);
+   $big_lamp = mysqli_real_escape_string($db, $_GET['big_lamp']);
+   $_password = mysqli_real_escape_string($db, $_GET['password']);
 
-       public function writing_to_DB() {
-          if ($small_lamp) {
-              $db->query("INSERT INTO `".DB_NAME."` (`small_lamp`) VALUES (".$small_lamp.");");
-          }
-          if ($pechka) {
-              $db->query("INSERT INTO `".DB_NAME."` (`pechka`) VALUES (".$pechka.");");
-          }
-          if ($big_lamp) {
-              $db->query("INSERT INTO `".DB_NAME."` (`big_lamp`) VALUES (".$big_lamp.");");
-          }
-          if ($_password) {
-              $db->query("INSERT INTO `".DB_NAME."` (`password`) VALUES (".$_password.");");
-          }
-       }
-
+   if ($small_lamp) {
+       $db->query("UPDATE `".DB_NAME."` SET `small_lamp` = ".$small_lamp.";");
+   }
+   if ($pechka) {
+       $db->query("UPDATE `".DB_NAME."` SET `pechka` = ".$pechka.";");
+   }
+   if ($big_lamp) {
+       $db->query("UPDATE `".DB_NAME."` SET `big_lamp` = ".$big_lamp.";");
+   }
+   if ($_password) {
+       $db->query("UPDATE `".DB_NAME."` SET `password` = ".$_password.";");
    }
 
    mysql_close($db);
